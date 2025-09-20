cd1f0803a851847e859fdfb02a16631856e7243c
Zorrobyte <ubercomputers@icloud.com>
2025-09-20 02:56:14 -0400

    try fixing dupes
    aa

--- rtspbrute/__init__.py
+++ rtspbrute/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 DEFAULT_ROUTES = Path(__file__).parent / "routes.txt"
 DEFAULT_CREDENTIALS = Path(__file__).parent / "credentials.txt"
